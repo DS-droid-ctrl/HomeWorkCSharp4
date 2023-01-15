14eca74a0f6c2daa7586c8adc17aa949c6a3fdf0
DenisM <mdr2010@yandex.ru>
2023-01-16 00:52:29 +0500

    Add Task27

---
+++ Task27/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе
+
+Console.Clear();
+Console.Write("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+if (a < 0)
+{
+    a *= -1;
+}
+int[] array = a.ToString().Select(t => int.Parse(t.ToString())).ToArray();
+int sum = 0;
+int size = array.Length;
+
+for (int i = 0; i < size; i++)
+{
+    sum = sum + array[i];
+}
+Console.WriteLine($"Суммма цифр в числе: {sum}");
